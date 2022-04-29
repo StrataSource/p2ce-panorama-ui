a79b9a88af0065f3941b27c9737ab866bf0031a6
Trico-Everfire <bobbys@live.nl>
2022-04-29 04:53:23 +0200

    completed Luke's concept for P2CE main menu. added functionality to Quit menu.

--- scripts/mainmenu/mainmenu_p2ce.js
+++ scripts/mainmenu/mainmenu_p2ce.js
@@ -4,11 +4,28 @@ class MainMenuP2CE {
 	static onShowMainMenu() {
 		$.DispatchEvent("P2CEMainMenuSetBackgroundMovie", "community_bg1");
 	}
+
+	static hideQuitMenu(bool = true) {
+		const panel = $("#QuitMenu");
+		if(!bool){
+			panel.RemoveClass('Disabled');
+			panel.AddClass('Enabled');
+			panel.RemoveClass('MainMenuSubContainerDisabled');
+			panel.AddClass('MainMenuSubContainerEnabled');
+		} else {
+			$.Schedule(0.2, ()=>panel.RemoveClass('Enabled'));
+			$.Schedule(0.2, ()=>panel.AddClass('Disabled'));
+			panel.RemoveClass('MainMenuSubContainerEnabled');
+			panel.AddClass('MainMenuSubContainerDisabled');
+		}
+	}
 	/*
 	static onShowPauseMenu() {
 	}
 	*/
 }
+
+
 
 (function () {
 	$.RegisterForUnhandledEvent("P2CEShowMainMenu", MainMenuP2CE.onShowMainMenu);
