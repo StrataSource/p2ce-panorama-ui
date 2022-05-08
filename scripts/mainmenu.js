6d916a399d681f7f348f3f1c50a4b64314021ee8
Trico-Everfire <bobbys@live.nl>
2022-05-08 02:05:44 +0200

    Near final menu for New Game.

--- scripts/mainmenu.js
+++ scripts/mainmenu.js
@@ -92,12 +92,20 @@ var MainMenuController = (function () {
 		let file = $.LoadKeyValuesFile( "panorama/config/test_map_config.kv" );
 		if(file != undefined){ //this happens if the file is both not present or if it's invalid KV.
 			for(let [MenuIdentifier,MenuContents] of Object.entries(file)){
-				let button = $.CreatePanel("Button", content, "", {onactivate:"MainMenuController.onNewgameSelected("+JSON.stringify(MenuContents.maps)+");",class: "CampaignButtonButton ListItem MainNewGameListItem"});
+				let button = $.CreatePanel("RadioButton", content, "", {class: "CampaignButtonButton ListItem MainNewGameListItem", group:"RBG1"});
+				button.SetPanelEvent("onactivate", (_)=>{
+					MainMenuController.onNewgameSelected(MenuContents.maps)
+					$("#MainMenuNewGameChapterDescriptionLabel").text = MenuContents.description;
+					$("#MainMenuNewGameChapterTitleLabel").text = MenuContents.name;
+					$("#MainMenuNewGameChapterTitle2Label").text = "By " + MenuContents.publisher;
+					$("#MainMenuNewGameCampaignImage").SetImage(MenuContents.icon);
+				});
 				$.CreatePanel("Label",button,"", {text: MenuContents.name, class:"CampaignButtonLabel CampaignButtonLabelPrimary"});
 				$.CreatePanel("Label",button,"", {text:"\nBy "+MenuContents.publisher, class:"CampaignButtonLabel CampaignButtonLabelSecondary"});
 				$.CreatePanel("Image",button,"", {src:MenuContents.icon, class:"CampaignButtonImage"});
 			}
 		} else {
+			//TODO: Write proper error handling for when the kv file is invalid/missing.
 			$.CreatePanel("Label", content, "", { style:"align: center center;", text: "OOPS!" });
 		}
 		_hideElement("#MainMenuButtonsNewGameContent",false)
@@ -105,11 +113,13 @@ var MainMenuController = (function () {
 
 	function _onNewgameSelected(maps) {
 		let content = $("#MainMenuButtonsNewGamePanel");
-		let contentImage = $("#MainMenuNewGameCampaignImage");
+		$("#MainMenuNewGameChapterImage").SetImage("");
+		$("#MainMenuNewGamePlayButton").enabled = false;
 		content.RemoveAndDeleteChildren();
 		_hideElement("#MainNewGamePanel",false)
 		for(let [MenuIdentifier,MenuContents] of Object.entries(maps)){
-			let button = $.CreatePanel("Button", content, "", {onactivate:"MainMenuController.onChapterSelected("+JSON.stringify(MenuContents)+");",class: "ListItem MainMenuChapterListItem"});
+			let button = $.CreatePanel("RadioButton", content, "", {group:"RBG2", class: "ListItem MainMenuChapterListItem"});
+			button.SetPanelEvent("onactivate",(_)=>MainMenuController.onChapterSelected(MenuContents))
 			$.CreatePanel("Label",button,"", {text: MenuContents.name, class:""});
 		}
 	}
@@ -118,10 +128,8 @@ var MainMenuController = (function () {
 		let content = $("#MainMenuNewGameChapterImage");
 		let playbutton = $("#MainMenuNewGamePlayButton");
 		playbutton.enabled = true;
-		//currentSelectedMap.data().onactivate = "MainMenuController.playMap('"+map.map+"');"
 		currentSelectedMap = map.map;
 		content.SetImage(map.image);
-		
 	}
 
 	function _playSelectedMap(){
