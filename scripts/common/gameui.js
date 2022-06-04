cefe8a52636a436e751d68a35441a5409f7dc35c
craftablescience <brendanelewis@gmail.com>
2022-06-04 01:24:46 -0400

    refactor: global styles are much cleaner

--- scripts/common/gameui.js
+++ scripts/common/gameui.js
@@ -1,10 +1,11 @@
-"use strict"
+/* eslint-disable no-unused-vars */
+"use strict";
 
 var GAME_UI_STATE = {
-    INVALID:0,
-    LOADINGSCREEN:1,
-    INGAME:2,
-    MAINMENU:3,
-    PAUSEMENU:4,
-    INTROMVIE:5
-}
+    INVALID:       0,
+    LOADINGSCREEN: 1,
+    INGAME:        2,
+    MAINMENU:      3,
+    PAUSEMENU:     4,
+    INTROMVIE:     5,
+};
