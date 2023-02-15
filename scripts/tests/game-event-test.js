0648d02dccf29b8885dd6403700fd97604cca469
JJL772 <jeremy.lorelli.1337@gmail.com>
2023-02-14 23:10:20 -0800

    feat: test for game events api

---
+++ scripts/tests/game-event-test.js
@@ -0,0 +1,28 @@
+
+class GameEventTests {
+	
+	static PlayerHurt(evName, data) {
+		$.Msg('Player hurt by ' + data.attacker + ' now has ' + data.health + ' health');
+	}
+
+	static PlayerSpawn(evName, data) {
+		$.Msg('Player ' + data.userid + ' spawned');
+	}
+
+	static EntityKilled(evName, data) {
+		$.Msg('Entity ' + data.entindex_killed + ' killed by ' + data.entindex_attacker);
+	}
+
+	static Dummy(evName, data) {
+		$.Msg('THIS MESSAGE SHOULD NOT SHOW');
+	}
+
+	static {
+		GameInterfaceAPI.RegisterGameEventHandler('entity_killed', GameEventTests.EntityKilled);
+		GameInterfaceAPI.RegisterGameEventHandler('player_spawn', GameEventTests.PlayerSpawn);
+		GameInterfaceAPI.RegisterGameEventHandler('player_hurt', GameEventTests.PlayerHurt);
+
+		const bad = GameInterfaceAPI.RegisterGameEventHandler('player_spawn', GameEventTests.Dummy);
+		GameInterfaceAPI.UnregisterGameEventHandler(bad);
+	}
+}
