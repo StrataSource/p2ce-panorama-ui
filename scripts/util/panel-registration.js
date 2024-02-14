f5bb24238f7b1d9ee70e78238ed7a1746191f821
Ostojλ <ostojasredojevic@protonmail.com>
2024-02-15 00:58:44 +0100

    Added armor and tweaked hud elements.

--- scripts/util/panel-registration.js
+++ scripts/util/panel-registration.js
@@ -1,7 +1,8 @@
-'use strict';
+"use strict";
 
 //--------------------------------------------------------------------------------------------------
 // Common place to register new panel type with panorama
 //--------------------------------------------------------------------------------------------------
 
-UiToolkitAPI.RegisterPanel2d('LineGraph', 'file://{resources}/layout/components/graphs/line-graph.xml');
+UiToolkitAPI.RegisterPanel2d("LineGraph", "file://{resources}/layout/components/graphs/line-graph.xml");
+UiToolkitAPI.RegisterPanel2d("ChaosHudArmor", "file://{resources}/layout/hud/armor.xml");
