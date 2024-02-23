7a50c42b584cc2049a5079bee5a0dd64a55c96e3
Jeremy Lorelli <jeremy.lorelli.1337@gmail.com>
2024-02-23 00:32:30 -0800

    feat: Initial VersionInfo component
    Co-authored-by: AWildErin <iansharp1@live.co.uk>

---
+++ scripts/components/version-info.js
@@ -0,0 +1,24 @@
+'use strict';
+
+class VersionInfo {
+	static {
+		$.GetContextPanel().jsClass = this;
+	}
+
+	static update() {
+		const cp = $.GetContextPanel();
+
+		const graphics = VersionAPI.GetGraphicsAPI();
+		const version = VersionAPI.GetVersion();
+		const branch = VersionAPI.GetBranch();
+		const platform = VersionAPI.GetPlatform();
+		const physics = VersionAPI.GetPhysicsEngine();
+
+		// Set the dialog variables so this can be used in label
+		cp.SetDialogVariable('graphics', graphics);
+		cp.SetDialogVariable('version', version);
+		cp.SetDialogVariable('branch', branch);
+		cp.SetDialogVariable('platform', platform);
+		cp.SetDialogVariable('physics', physics);
+	}
+}
