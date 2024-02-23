26a50aaec10b1956112af63bd897da2301ee12fc
Jeremy Lorelli <jeremy.lorelli.1337@gmail.com>
2024-02-23 00:32:30 -0800

    feat: Initial VersionInfo component
    Co-authored-by: AWildErin <iansharp1@live.co.uk>

--- scripts/util/panel-registration.js
+++ scripts/util/panel-registration.js
@@ -5,3 +5,4 @@
 //--------------------------------------------------------------------------------------------------
 
 UiToolkitAPI.RegisterPanel2d('LineGraph', 'file://{resources}/layout/components/graphs/line-graph.xml');
+UiToolkitAPI.RegisterPanel2d('VersionInfo', 'file://{resources}/layout/components/version-info.xml');
