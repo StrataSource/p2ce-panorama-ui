5e8cb7210706fd5e98b24fb359c41c5d8b44fd88
Jeremy Lorelli <jeremy.lorelli.1337@gmail.com>
2024-07-14 23:52:53 -0700

    Bunch of crappy UI work + workshop UI/API test

---
+++ scripts/pages/main-menu/campaigns.ts
@@ -0,0 +1,12 @@
+'use strict';
+
+class CampaignSelector {
+	static campaignCarousel = $<Carousel>("#CampaignCarousel");
+
+	static init() {
+		if (!this.campaignCarousel) return;
+		for (let i = 0; i < 16; ++i) {
+			$.CreatePanel('Label', this.campaignCarousel, 'test'+i, {'text': 'sussy ' + i, 'class': 'campaignselector__item'});
+		}
+	}
+}
