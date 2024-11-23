1f48f71a8ad10f5f5ed5825814b1dcb3cddacc2e
Jeremy Lorelli <jeremy.lorelli.1337@gmail.com>
2024-11-23 01:01:23 -0800

    feat: yet more tweaks and cleanup; in a decent (but ugly) state now

--- scripts/pages/main-menu/addons.ts
+++ scripts/pages/main-menu/addons.ts
@@ -47,11 +47,38 @@ class AddonEntry {
 	}
 
 	/**
+	 * Update enabled state from Workshop API
+	 */
+	updateEnabled() {
+		this.addonEnableCheck?.SetSelected(WorkshopAPI.GetAddonEnabled(this.index));
+	}
+
+	/**
+	 * Returns if the addon is enabled or not
+	 */
+	isEnabled(): boolean {
+		if (this.addonEnableCheck)
+			return this.addonEnableCheck.IsSelected();
+		return false;
+	}
+
+	/**
 	 * Toggle an addon's enable state
 	 */
 	addonToggle() {
-		if (this.addonEnableCheck)
-			WorkshopAPI.SetAddonEnabled(this.index, this.addonEnableCheck.IsSelected());
+		AddonManager.markDirty();
+	}
+
+	/**
+	 * Enable or disable addon (in UI only)
+	 * Marks the addon list dirty
+	 */
+	setAddonEnabled(en: boolean) {
+		if (this.addonEnableCheck) {
+			if (this.addonEnableCheck.IsSelected() != en)
+				AddonManager.markDirty();
+			this.addonEnableCheck.SetSelected(en);
+		}
 	}
 
 	/**
@@ -69,11 +96,16 @@ class AddonEntry {
 class AddonManager {
 	static addonContainer = $<Panel>('#AddonContainer');
 	static addonPanel = $<Panel>('#AddonPanel');
-	static addonTitle = this.addonPanel?.FindChildTraverse('AddonTitle') as Label;
-	static addonDesc = this.addonPanel?.FindChildTraverse('AddonDesc') as Label;
-	static addonAuthors = this.addonPanel?.FindChildTraverse('AddonAuthors') as Label;
+	static addonTitle = this.addonPanel?.FindChildTraverse<Label>('AddonTitle');
+	static addonDesc = this.addonPanel?.FindChildTraverse<Label>('AddonDesc');
+	static addonAuthors = this.addonPanel?.FindChildTraverse<Label>('AddonAuthors');
+
+	static applyButton = $<Button>('#ApplyButton');
+	static cancelButton = $<Button>('#CancelButton');
+	static toggleAllButton = $<ToggleButton>('#ToggleAll');
 
 	static addons: AddonEntry[] = [];
+	static dirty: boolean = false;
 
 	static init() {
 		$.RegisterForUnhandledEvent('LayoutReloaded', this.reloadCallback);
@@ -81,9 +113,9 @@ class AddonManager {
 	}
 
 	static updateAddons() {
-		this.addons.forEach(element => {
-			element.update();
-		});
+		for (const addon of this.addons) {
+			addon.update();
+		}
 	}
 
 	/**
@@ -95,13 +127,21 @@ class AddonManager {
 		this.purgeAddonList();
 
 		const addonCount = WorkshopAPI.GetAddonCount();
+		let anyEnabled = false;
 		for (let i = 0; i < addonCount; ++i) {
 			const panel = $.CreatePanel('Panel', this.addonContainer, 'addon'+i);
 			panel.SetPanelEvent('onactivate', () => this.addonSelected(i));
 			panel.LoadLayoutSnippet('AddonEntrySnippet');
 
+			if (WorkshopAPI.GetAddonEnabled(i))
+				anyEnabled = true;
+
 			this.addons.push(new AddonEntry(i, panel));
 		}
+
+		// If any addons are enabled, we'll default the "select all" button to true
+		if (this.toggleAllButton)
+			this.toggleAllButton.SetSelected(anyEnabled);
 
 		this.updateAddons();
 	}
@@ -113,24 +153,86 @@ class AddonManager {
 	static addonSelected(addon: number) {
 		const info = WorkshopAPI.GetAddonMeta(addon);
 
-		this.addonTitle.text = info.title;
-		this.addonDesc.text = info.description;
-		if (info.authors.length > 0) {
+		if (this.addonTitle)
+			this.addonTitle.text = info.title;
+
+		if (this.addonDesc)
+			this.addonDesc.text = info.description;
+
+		if (info.authors.length > 0 && this.addonAuthors) {
 			this.addonAuthors.text = 'by ' + info.authors.join(', ');
 			this.addonAuthors.visible = true;
 		}
-		else {
+		else if (this.addonAuthors) {
 			this.addonAuthors.text = '';
 			this.addonAuthors.visible = false;
 		}
 	}
 
-	static enableAll() {
+	/**
+	 * Signal that a change to the addon list has been made
+	 */
+	static markDirty(dirty: boolean = true) {
+		this.dirty = dirty;
 
+		if (this.cancelButton)
+			this.cancelButton.enabled = this.dirty;
+
+		if (this.applyButton)
+			this.applyButton.enabled = this.dirty;
 	}
 
-	static disableAll() {
+	/**
+	 * Apply changes to the addon list
+	 */
+	static apply() {
+		if (!this.dirty) return;
+		$.Msg('Applying addon changes');
 
+		const enableList = {};
+		for (const addon of this.addons) {
+			enableList[addon.index] = addon.isEnabled();
+		}
+		WorkshopAPI.SetAddonListEnabled(enableList);
+		this.markDirty(false);
+
+		// Update UI to reflect the new enable/disable state
+		for (const addon of this.addons) {
+			addon.updateEnabled();
+		}
+	}
+
+	/**
+	 * Cancel changes made to the addon list
+	 */
+	static cancel() {
+		if (!this.dirty) return;
+
+		$.Msg('Reverting addon changes');
+
+		let anyEnabled = false;
+		for (const addon of this.addons) {
+			addon.updateEnabled();
+			if (addon.isEnabled())
+				anyEnabled = true;
+		}
+
+		this.markDirty(false);
+
+		if (this.toggleAllButton)
+			this.toggleAllButton.SetSelected(anyEnabled);
+	}
+
+	/**
+	 * Toggle all addons on or off
+	 */
+	static toggleAll() {
+		if (!this.toggleAllButton) return;
+
+		const enable = this.toggleAllButton.IsSelected();
+		for (const addon of this.addons) {
+			addon.setAddonEnabled(enable);
+		}
 	}
 
 	static purgeAddonList() {
@@ -142,5 +244,4 @@ class AddonManager {
 		this.purgeAddonList();
 		this.createAddonEntries();
 	}
-
 }
