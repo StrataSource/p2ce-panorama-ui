5c0c8b43320ad4dd0e80fef43a6b78f82de25f9a
Jeremy Lorelli <jeremy.lorelli.1337@gmail.com>
2025-06-20 00:39:52 -0700

    scripts: Add missing event definitions

--- scripts/types-p2ce/events.d.ts
+++ scripts/types-p2ce/events.d.ts
@@ -3,4 +3,18 @@
  */
 interface GlobalEventNameMap {
 	ReloadBackground: () => void;
+	HideContentPanel: () => void;
+	ShowContentPanel: () => void;
+	MainMenuTabShown: (tabid) => void;
+	MainMenuTabHidden: (tabid) => void;
+	SettingsNavigateToPanel: (category, settingPanel) => void;
+	SettingsSave: () => void;
+	ExtendDrawer: () => void;
+	RetractDrawer: () => void;
+	ToggleDrawer: () => void;
+	Drawer_UpdateLobbyButton: (imagesrc, playercount) => void;
+	Drawer_NavigateToTab: (tabid) => void;
+	Drawer_ExtendAndNavigateToTab: (tabid) => void;
+	ColorPickerSave: (color) => void;
+	ColorPickerCancel: () => void;
 }
