c08b4823f6500f441bc00d099318d79aba0aa7f9
sirenstorm <120330118+iamsirenstorm@users.noreply.github.com>
2025-11-24 00:17:50 -0800

    feat: campaign settings opt-in

--- scripts/pages/campaigns/campaign-chapters.ts
+++ scripts/pages/campaigns/campaign-chapters.ts
@@ -2,11 +2,11 @@
 
 class ChapterEntry {
 	index: number;
-	panel: Button;
+	panel: RadioButton;
 	chapter: ChapterInfo;
 	locked: boolean;
 
-	constructor(index: number, panel: Button, chapter: ChapterInfo, locked: boolean) {
+	constructor(index: number, panel: RadioButton, chapter: ChapterInfo, locked: boolean) {
 		this.index = index;
 		this.panel = panel;
 		this.chapter = chapter;
@@ -29,7 +29,8 @@ class ChapterEntry {
 		}
 
 		this.panel.SetPanelEvent('onactivate', () => {
-			CampaignMgr.selectChapter(this.chapter);
+			CampaignChaptersTab.selectedChapter = this.chapter;
+			CampaignChaptersTab.starterPanel.enabled = true;
 		});
 	}
 }
@@ -40,6 +41,8 @@ class CampaignChaptersTab {
 	static campaignLister = $<Panel>('#CampaignLister')!;
 	static tabLabel = $<Label>('#CampaignListerModeLabel')!;
 	static chapterEntries: ChapterEntry[] = [];
+	static starterPanel = $<Panel>('#CampaignStarterPanel')!;
+	static selectedChapter: ChapterInfo;
 
 	static setActive() {
 		CampaignSavesTab.close();
@@ -49,10 +52,13 @@ class CampaignChaptersTab {
 	}
 
 	static close() {
+		this.starterPanel.visible = false;
 		this.purgeChapterList();
 	}
 
 	static show() {
+		this.starterPanel.enabled = false;
+		this.starterPanel.visible = true;
 		this.campaignControls.visible = false;
 		this.campaignListerContainer.visible = true;
 		this.tabLabel.text = $.Localize('#MainMenu_Campaigns_MM_NewGame');
@@ -66,7 +72,7 @@ class CampaignChaptersTab {
 		const chapters = CampaignMgr.currentCampaign!.chapters;
 
 		for (let i = 0; i < chapters.length; ++i) {
-			const p = $.CreatePanel('Button', this.campaignLister, 'chapter' + i, {
+			const p = $.CreatePanel('RadioButton', this.campaignLister, 'chapter' + i, {
 				class: i !== chapters.length - 1 ? 'chapters__entry__lined' : ''
 			});
 			p.LoadLayoutSnippet('ChapterEntrySnippet');
@@ -74,5 +80,30 @@ class CampaignChaptersTab {
 			this.chapterEntries.push(new ChapterEntry(i, p, chapters[i], false));
 			this.chapterEntries[i].update();
 		}
+	}
+
+	static startChapter() {
+		if (GameInterfaceAPI.GetGameUIState() === GameUIState.MAINMENU) {
+			CampaignMgr.selectChapter(this.selectedChapter);
+			CampaignMgr.startGame();
+		} else {
+			UiToolkitAPI.ShowGenericPopupTwoOptionsBgStyle(
+				$.Localize('#Action_NewGame_Title'),
+				$.Localize('#Action_NewGame_Description'),
+				'warning-popup',
+				$.Localize('#UI_Yes'),
+				() => {
+					CampaignMgr.selectChapter(this.selectedChapter);
+					CampaignMgr.startGame();
+				},
+				$.Localize('#UI_Cancel'),
+				() => {},
+				'blur'
+			);
+		}
+	}
+
+	static customizeChapter() {
+		CampaignMgr.customizeChapter(this.selectedChapter);
 	}
 }
