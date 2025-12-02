f708928248008698ae28532bdbf2ddf82c78a95e
sirenstorm <120330118+iamsirenstorm@users.noreply.github.com>
2025-12-02 09:28:01 -0800

    feat: campaign settings

--- scripts/types-p2ce/ui-state-types.d.ts
+++ scripts/types-p2ce/ui-state-types.d.ts
@@ -9,3 +9,10 @@ declare const enum GameType {
 	PORTAL2_MAP = 2,
 	LOOSE_MAP = 3
 }
+
+declare const enum GlobalUiObjects {
+	UI_ACTIVE_CHAPTER = 'ActiveUiChapter',
+	UI_ACTIVE_CAMPAIGN = 'ActiveUiCampaign',
+	UI_CAMPAIGN_SETTINGS = 'UiCampaignSettings',
+	UI_CAMPAIGN_SETTING_PAGE = 'UiCampaignSettingPage',
+}
