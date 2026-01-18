91cb3bc1431e691ddb91ca511547c9d87332aaba
iamsirenstorm <120330118+iamsirenstorm@users.noreply.github.com>
2026-01-17 19:54:15 -0600

    feat: partial rewrite of menu system

---
+++ scripts/pages/main-menu/menu-manager.ts
@@ -0,0 +1,393 @@
+'use strict';
+
+class MenuPage {
+	name: string;
+	panel: Panel;
+	headline?: string;
+	tagline?: string;
+	invokerPanel?: Panel;
+
+	constructor(name: string, panel: Panel) {
+		this.name = name;
+		this.panel = panel;
+	}
+
+	setLines(headline: string, tagline: string) {
+		this.headline = headline;
+		this.tagline = tagline;
+	}
+}
+
+class MenuManager {
+	static menuNav = $('#MenuNav')!;
+	static menuBackground = $('#MenuModeBackgroundLayer')!;
+	static menuForeground = $('#MenuModeForegroundLayer')!;
+	static page = $<Panel>('#PageWrap')!;
+	static pageBg = $<Panel>('#PageBg')!;
+	static pageHeadline = $<Label>('#PageHeadline')!;
+	static pageTagline = $<Label>('#PageTagline')!;
+	static pageInsert = $<Panel>('#PageInsert')!;
+	static pageActions = $<Panel>('#PageActions')!;
+	static gradientBar = $<Panel>('#GradientBar')!;
+	static controls = $<Panel>('#MenuRootControls')!;
+	static logo = $<Image>('#GameFullLogo')!;
+	static loadingIndicator = $<Label>('#LoadingIndicator')!;
+	static menuContent = $<Panel>('#MenuMainContent')!;
+
+	static pages: MenuPage[] = [];
+
+	static onLoaded() {
+		// this is for campaign-menu, see there for more info
+		UiToolkitAPI.GetGlobalObject()['FirstCampaignLoaded'] = false;
+
+		$.RegisterForUnhandledEvent(
+			'MainMenuAddButton',
+			(btn: MenuButton) => {
+				const b = constructMenuButton(btn);
+				b.SetParent(this.menuNav);
+				b.SetReadyForDisplay(true);
+				this.updateFocus();
+			}
+		);
+
+		$.RegisterForUnhandledEvent(
+			'MainMenuAddPreConstructedButton',
+			(btn: GenericPanel) => {
+				btn.SetParent(this.menuNav);
+				btn.SetReadyForDisplay(true);
+				this.updateFocus();
+			}
+		);
+
+		$.RegisterForUnhandledEvent(
+			'MainMenuAddBgPanel',
+			(panel: Panel) => {
+				panel.SetParent(this.menuBackground);
+				panel.SetReadyForDisplay(true);
+			}
+		);
+
+		$.RegisterForUnhandledEvent(
+			'ShowMainMenu',
+			() => {
+				this.menuContent.AddClass('mainmenu__menu__t-prop');
+				this.menuContent.RemoveClass('mainmenu__menu__anim');
+				this.openMenuMode();
+			}
+		)
+
+		$.RegisterForUnhandledEvent(
+			'HideMainMenu',
+			() => {
+				this.deleteMenus();
+				this.menuContent.RemoveClass('mainmenu__menu__t-prop');
+				this.menuContent.AddClass('mainmenu__menu__anim');
+			}
+		);
+
+		$.RegisterForUnhandledEvent(
+			'ShowPauseMenu',
+			() => {
+				this.menuContent.AddClass('mainmenu__menu__t-prop');
+				this.menuContent.RemoveClass('mainmenu__menu__anim');
+				this.openMenuMode();
+			}
+		);
+
+		$.RegisterForUnhandledEvent(
+			'HidePauseMenu',
+			() => {
+				this.deleteMenus();
+				this.menuContent.RemoveClass('mainmenu__menu__t-prop');
+				this.menuContent.AddClass('mainmenu__menu__anim');
+			}
+		);
+
+		$.RegisterForUnhandledEvent(
+			'MainMenuOpenNestedPage',
+			this.navigateToPage.bind(this)
+		);
+
+		$.RegisterForUnhandledEvent(
+			'MainMenuSetPageLines',
+			this.onMenuSetPageLines.bind(this)
+		);
+
+		$.RegisterForUnhandledEvent(
+			'MainMenuCloseAllPages',
+			this.closePages.bind(this)
+		);
+
+		$.RegisterEventHandler(
+			'Cancelled',
+			$.GetContextPanel(),
+			() => {
+				// Resume game in pause menu mode
+				if (GameInterfaceAPI.GetGameUIState() === GameUIState.PAUSEMENU) {
+					// Has to be on the root menu in order to resume
+					if (this.pages.length > 0) this.navigateBack();
+					else $.DispatchEvent('MainMenuResumeGame');
+				} else {
+					// Exit current page
+					this.navigateBack();
+				}
+			}
+		);
+
+		$.RegisterForUnhandledEvent(
+			'MainMenuSetLogo',
+			(logo: string) => {
+				if (logo.length > 0) {
+					this.logo.SetImage(`file://${logo}`);
+				} else {
+					this.logo.SetImage(getRandomFallbackImage());
+				}
+			}
+		);
+
+		MenuAnimation.init();
+
+		this.loadingIndicator.visible = true;
+		$.DispatchEvent('MainMenuSwitchFade', true, true);
+
+		if (GameInterfaceAPI.GetSettingString('campaign_default').length === 0) {
+			$.RegisterForUnhandledEvent(
+				'PanoramaComponent_Campaign_OnActiveCampaignChanged',
+				(campaign: string | null) => {
+					this.closePages();
+					$.DispatchEvent('MainMenuSwitchFade', false, true);
+					this.deleteMenus();
+					this.openMenuMode();
+				}
+			);
+			this.openMenuMode();
+		} else {
+			$.Schedule(0.1, () => {
+				$.RegisterForUnhandledEvent(
+					'PanoramaComponent_Campaign_OnActiveCampaignChanged',
+					(campaign: string | null) => {
+						this.closePages();
+						$.DispatchEvent('MainMenuSwitchFade', false, true);
+						this.deleteMenus();
+						this.openMenuMode();
+					}
+				);
+				this.openMenuMode();
+			});
+		}
+	}
+
+	static updateFocus() {
+		const c = this.menuNav.Children();
+		if (c.length > 0) {
+			c[0].SetFocus();
+		}
+	}
+
+	static deleteMenus() {
+		this.menuNav.RemoveAndDeleteChildren();
+		this.menuBackground.RemoveAndDeleteChildren();
+		this.menuForeground.RemoveAndDeleteChildren();
+	}
+
+	static openMenuMode() {
+		switch (GameInterfaceAPI.GetGameUIState()) {
+			case GameUIState.MAINMENU:
+				{
+					if (!CampaignAPI.IsCampaignActive()) {
+						const p = $.CreatePanel('Panel', this.menuForeground, 'MenuMode_MainMenu');
+						p.LoadLayout('file://{resources}/layout/pages/main-menu/base-menu.xml', false, false);
+						p.SetReadyForDisplay(true);
+					} else {
+						const p = $.CreatePanel('Panel', this.menuForeground, 'MenuMode_CampaignMenu');
+						p.LoadLayout('file://{resources}/layout/pages/main-menu/campaign-menu.xml', false, false);
+						p.SetReadyForDisplay(true);
+					}
+					break;
+				}
+
+			case GameUIState.PAUSEMENU:
+				{
+					const p = $.CreatePanel('Panel', this.menuForeground, 'MenuMode_PauseMenu');
+					p.LoadLayout('file://{resources}/layout/pages/main-menu/pause-menu.xml', false, false);
+					p.SetReadyForDisplay(true);
+					break;
+				}
+		
+			default:
+				$.Warning("Don't know which menu to open for this UI state! Doing nothing!");
+				break;
+		}
+	}
+
+	static onMenuSetPageLines(headline: string, tagline: string) {
+		this.pageHeadline.text = headline;
+		this.pageTagline.text = tagline;
+
+		if (this.pages.length > 0) {
+			this.pages[this.pages.length - 1].setLines(headline, tagline);
+		}
+	}
+
+	// animate a 'flash' on the page header texts
+	static flashPageLines() {
+		// TODO: use keyframe anims
+		//const kfs = this.pageHeadline.CreateCopyOfCSSKeyframes('FadeIn');
+		//this.pageHeadline.UpdateCurrentAnimationKeyframes(kfs);
+		//this.pageTagline.UpdateCurrentAnimationKeyframes(kfs);
+
+		// remove the property setting the transitions
+		this.pageHeadline.RemoveClass('mainmenu__page-controls__t-prop');
+		this.pageTagline.RemoveClass('mainmenu__page-controls__t-prop');
+		// set the blur
+		this.pageHeadline.AddClass('mainmenu__page-controls__flash');
+		this.pageTagline.AddClass('mainmenu__page-controls__flash');
+		// return the transition properties and remove the blur
+		$.Schedule(0.001, () => {
+			this.pageHeadline.AddClass('mainmenu__page-controls__t-prop');
+			this.pageTagline.AddClass('mainmenu__page-controls__t-prop');
+			this.pageHeadline.RemoveClass('mainmenu__page-controls__flash');
+			this.pageTagline.RemoveClass('mainmenu__page-controls__flash');
+		});
+	}
+
+	// open a page, handles nested pages and receives calls via events from other pages
+	// note: page headline/tagline are set by the corresponding page script, not here
+	static navigateToPage(tab: string, xmlName: string, invokerPanel?: Panel) {
+		if (
+			this.pages.find((v: MenuPage) => {
+				return v.name === tab;
+			})
+		) {
+			$.Warning(`Page '${tab}' already exists, rejecting request to open the page.`);
+			return;
+		}
+
+		// hide the previous page
+		if (this.pages.length > 0) {
+			const priorPage = this.pages[this.pages.length - 1];
+			if (priorPage.panel.IsValid()) {
+				//priorPage.panel.visible = false;
+				priorPage.panel.AddClass('mainmenu__page__back-anim');
+			}
+		}
+
+		// create the new page
+		const newPanel = $.CreatePanel('Panel', this.pageInsert, tab);
+		const newPage = new MenuPage(tab, newPanel);
+		newPage.invokerPanel = invokerPanel;
+		this.pages.push(newPage);
+		newPanel.LoadLayout(`file://{resources}/layout/pages/${xmlName}.xml`, false, false);
+		newPanel.RegisterForReadyEvents(true);
+		newPanel.AddClass('mainmenu__page__anim');
+		newPanel.SetFocus();
+
+		stripDevTagsFromLabels(newPanel);
+
+		// hide menu elements, only done on root level
+		if (this.pages.length === 1) {
+			this.showPage();
+		} else {
+			// 'flash' the pagelines
+			this.flashPageLines();
+		}
+	}
+
+	// show page container
+	static showPage() {
+		this.controls.AddClass('mainmenu__nav__anim');
+		this.page.hittest = true;
+		this.page.hittestchildren = true;
+
+		this.pageHeadline.AddClass('mainmenu__page-controls__anim');
+		this.pageTagline.AddClass('mainmenu__page-controls__anim');
+		this.pageActions.AddClass('mainmenu__page-controls__anim');
+
+		this.gradientBar.AddClass('mainmenu__gradient-bar__anim');
+		this.page.AddClass('mainmenu__normal-page-container');
+		this.page.RemoveClass('mainmenu__wide-page-container');
+		this.pageBg.style.animation = 'FadeOut 0.2s ease-out 0s 1 reverse forwards';
+		this.menuForeground.style.animation = 'FadeOut 0.1s linear 0s 1 normal forwards';
+	}
+
+	// hide page container
+	static hidePage() {
+		this.controls.RemoveClass('mainmenu__nav__anim');
+		this.pageHeadline.RemoveClass('mainmenu__page-controls__anim');
+		this.pageTagline.RemoveClass('mainmenu__page-controls__anim');
+		this.pageActions.RemoveClass('mainmenu__page-controls__anim');
+
+		this.page.hittest = false;
+		this.page.hittestchildren = false;
+		this.gradientBar.RemoveClass('mainmenu__gradient-bar__anim');
+		this.pageBg.style.animation = 'FadeOut 0.2s ease-out 0s 1 normal forwards';
+		this.menuForeground.style.animation = 'FadeIn 0.25s linear 0s 1 normal forwards';
+	}
+
+	static reversePageAnim(panel: GenericPanel) {
+		$.RegisterEventHandler('PropertyTransitionEnd', panel, (panelName, propertyName) => {
+			if (panel.id === panelName && propertyName === 'transform') {
+				if (panel.IsTransparent()) {
+					panel.DeleteAsync(0);
+				}
+			}
+		});
+		panel.RemoveClass('mainmenu__page__anim');
+	}
+
+	// pop top page, returning to main menu if applicable
+	static navigateBack() {
+		if (this.pages.length === 0) return;
+
+		// delete the current page
+		const currentPage = this.pages.pop();
+		let noResetFocus = false;
+
+		if (currentPage) {
+			$.DispatchEvent('MainMenuPagePreClose', currentPage.name);
+			if (currentPage.name === 'Settings') $.DispatchEvent('SettingsSave');
+			if (currentPage.panel.IsValid()) this.reversePageAnim(currentPage.panel);
+
+			if (
+				currentPage.invokerPanel &&
+				currentPage.invokerPanel.IsValid() &&
+				currentPage.invokerPanel.visible &&
+				currentPage.invokerPanel.enabled
+			) {
+				noResetFocus = true;
+				currentPage.invokerPanel.SetFocus();
+				$.Msg(`Setting focus to ${currentPage.invokerPanel.id}`);
+			}
+		}
+
+		if (this.pages.length > 0) {
+			const nowPage = this.pages[this.pages.length - 1];
+
+			if (!nowPage.panel.IsValid()) return;
+
+			// set directly to avoid placing the lines back haha
+			if (nowPage?.headline && nowPage?.tagline) {
+				this.pageHeadline.text = nowPage.headline;
+				this.pageTagline.text = nowPage.tagline;
+			}
+
+			// restore the lower level page
+			nowPage.panel.RemoveClass('mainmenu__page__back-anim');
+			this.flashPageLines();
+			nowPage.panel.SetFocus();
+		} else {
+			// no more pages
+			this.hidePage();
+			$.DispatchEvent('MainMenuFullBackNav');
+
+			if (!noResetFocus) $.DispatchEvent('MainMenuSetFocus');
+		}
+	}
+
+	// pops all pages
+	static closePages() {
+		while (this.pages.length > 0) {
+			this.navigateBack();
+		}
+	}
+}
