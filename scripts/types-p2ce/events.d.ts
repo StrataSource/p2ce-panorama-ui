61c7490eee52ecda6ed114018d005b44db0747bf
iamsirenstorm <120330118+iamsirenstorm@users.noreply.github.com>
2026-01-17 19:54:15 -0600

    feat: partial rewrite of menu system

--- scripts/types-p2ce/events.d.ts
+++ scripts/types-p2ce/events.d.ts
@@ -10,10 +10,22 @@ interface GlobalEventNameMap {
 	MainMenuOpenNestedPage: (tab: string, xmlName: string, invokerPanel: Panel | undefined) => void;
 	MainMenuCloseAllPages: () => void;
 	MainMenuSetPageLines: (headline: string, tagline: string) => void;
-	MainMenuSwitchFade: () => void;
+	MainMenuSwitchFade: (instantFade?: boolean, instantMenu?: boolean) => void;
+	MainMenuSwitchReverse: (instant?: boolean) => void;
 	MainMenuPagePreClose: (tab: string) => void;
 	MainMenuSetFocus: () => void;
 	CampaignSettingHovered: (helpText: string) => void;
 	MainMenuFullBackNav: () => void;
 	MainBackgroundLoaded: () => void;
+	MainMenuAddPreConstructedButton: (panel: GenericPanel) => void;
+	MainMenuAddButton: (btn: MenuButton) => void;
+	MainMenuSetLogo: (logo: string) => void;
+	MainMenuSetLoadingIndicatorVisibility: (visible: boolean) => void;
+	MainMenuShowBackgroundImage: (img?: string, instant?: boolean) => void;
+	MainMenuHideBackgroundImage: (instant?: boolean) => void;
+	MainMenuShowBackgroundMovie: (src: string) => void;
+	MainMenuHideBackgroundMovie: () => void;
+	MainMenuHideNav: (instant?: boolean) => void;
+	MainMenuShowNav: (instant?: boolean) => void;
+	MainMenuAddBgPanel: (panel: Panel) => void;
 }
