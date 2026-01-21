0089f335db2d452863782948e94f4e45136b2af9
iamsirenstorm <120330118+iamsirenstorm@users.noreply.github.com>
2026-01-21 00:32:42 -0600

    feat: initial "fast switch style" weapon switcher script

--- scripts/util/panel-registration.ts
+++ scripts/util/panel-registration.ts
@@ -7,3 +7,4 @@
 UiToolkitAPI.RegisterPanel2d('LineGraph', 'file://{resources}/layout/components/graphs/line-graph.xml');
 UiToolkitAPI.RegisterPanel2d('VersionInfo', 'file://{resources}/layout/components/version-info.xml');
 UiToolkitAPI.RegisterPanel2d('NewsReel', 'file://{resources}/layout/pages/main-menu/news.xml');
+UiToolkitAPI.RegisterHUDPanel2d('WeaponSwitcher', 'file://{resources}/layout/hud/weapon-switcher.xml');
