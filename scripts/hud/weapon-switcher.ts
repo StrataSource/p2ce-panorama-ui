e3d007ed3f1cc440d6d295453ad1faf8c45a4f68
iamsirenstorm <120330118+iamsirenstorm@users.noreply.github.com>
2026-01-21 15:16:52 -0600

    feat: hl2-style (-ish) weapon hotbar

--- scripts/hud/weapon-switcher.ts
+++ scripts/hud/weapon-switcher.ts
@@ -1,5 +1,18 @@
 'use strict';
 
+class SwitcherData {
+	weapon: Weapon;
+	panel: Panel | undefined = undefined;
+
+	constructor(w: Weapon) {
+		this.weapon = w;
+	}
+
+	setPanel(p: Panel) {
+		this.panel = p;
+	}
+}
+
 class WeaponSwitcher {
 	static {
 		$.RegisterForUnhandledEvent('WeaponSelect', this.weaponSelected.bind(this));
@@ -7,43 +20,112 @@ class WeaponSwitcher {
 		$.RegisterForUnhandledEvent('LevelInitPostEntity', this.onLevelInit.bind(this));
 		$.RegisterForUnhandledEvent('LayoutReloaded', () => {
 			this.updateWeapons();
+			this.hide();
 		});
 	}
 
 	static weapons: (Weapon | null)[] | undefined = [];
+	static bucketedWeapons: SwitcherData[][] = [];
+	static selectedIndex: number[] = [0, 0];
+	static fadeTimer: uuid | undefined = undefined;
 
 	static onLoad() {
-		
 	}
 
 	static onLevelInit() {
+		// force refresh, for saved games
+		$.Schedule(0.1, () => {
+			this.updateWeapons();
+			this.hide();
+		});
+	}
 
+	static setIndexSelectionState(selected: boolean) {
+		if (this.bucketedWeapons[this.selectedIndex[0]]) {
+			const info = this.bucketedWeapons[this.selectedIndex[0]][this.selectedIndex[1]];
+			if (info) {
+				if (info.panel && info.panel.IsValid()) {
+					// expand the whole bucket
+					for (const pod of this.bucketedWeapons[this.selectedIndex[0]]) {
+						if (pod.panel && pod.panel.IsValid()) {
+							pod.panel.SetHasClass('weapons__bucket__entry__expanded', selected);
+						}
+					}
+					// make THIS one glowy
+					info.panel.SetHasClass('weapons__bucket__entry__selected', selected);
+				} else {
+					$.Warning(`WEAPON SELECT: Data for '${info.weapon.classname}' exists, but the panel is invalid`);
+				}
+			} else {
+				$.Warning(`WEAPON SELECT: index pair 2, ${this.selectedIndex[1]}, is invalid`);
+			}
+		} else {
+			$.Warning(`WEAPON SELECT: index pair 1, ${this.selectedIndex[0]}, is invalid`);
+		}
+	}
+
+	static show() {
+		$.GetContextPanel().style.animation = 'FadeIn 0.01s ease-out 0s 1 normal forwards';
+	}
+
+	static hide() {
+		$.GetContextPanel().style.animation = 'FadeOut 0.01s ease-out 0s 1 normal forwards';
+		const kfs = $.GetContextPanel().CreateCopyOfCSSKeyframes('FadeOut');
+		$.GetContextPanel().UpdateCurrentAnimationKeyframes(kfs);
+	}
+
+	static fadeOutTimer() {
+		if (this.fadeTimer !== undefined) $.CancelScheduled(this.fadeTimer);
+		this.fadeTimer = $.Schedule(0.5, () => {
+			$.GetContextPanel().style.animation = 'FadeOut 1.5s ease-out 0s 1 normal forwards';
+			const kfs = $.GetContextPanel().CreateCopyOfCSSKeyframes('FadeOut');
+			$.GetContextPanel().UpdateCurrentAnimationKeyframes(kfs);
+			this.fadeTimer = undefined;
+		});
 	}
 
 	static weaponSelected(action: WeaponSelectAction) {
 		$.Msg(`WeaponSelect: ${action}`);
 		if (!this.weapons) return;
 		//$.Msg(`${JSON.stringify(WeaponsAPI.GetWeapons())}`);
+		this.show();
+		$.PlaySoundEvent('Player.WeaponSelectionMoveSlot');
 		switch (action) {
 			case WeaponSelectAction.NEXT:
 				{
-					const index = WeaponsAPI.GetActiveWeapon() + 1;
-					if (index >= this.weapons.length) {
-						WeaponsAPI.SwitchToWeapon(0);
-					} else {
-						WeaponsAPI.SwitchToWeapon(index);
+					this.setIndexSelectionState(false);
+					this.selectedIndex[1] += 1;
+					if (this.selectedIndex[1] >= this.bucketedWeapons[this.selectedIndex[0]].length) {
+						this.selectedIndex[0] += 1;
+						this.selectedIndex[1] = 0;
+						if (this.selectedIndex[0] >= this.bucketedWeapons.length) {
+							this.selectedIndex[0] = 0;
+						}
 					}
+					const weapon = this.bucketedWeapons[this.selectedIndex[0]][this.selectedIndex[1]].weapon;
+					$.Msg(`Select Weapon: ${weapon.classname}`);
+					this.setIndexSelectionState(true);
+					WeaponsAPI.SwitchToWeapon(WeaponsAPI.GetWeaponIndexFromClass(weapon.classname));
+					this.fadeOutTimer();
 					break;
 				}
 
 			case WeaponSelectAction.PREV:
 				{
-					const index = WeaponsAPI.GetActiveWeapon() - 1;
-					if (index < 0) {
-						WeaponsAPI.SwitchToWeapon(this.weapons.length - 1);
-					} else {
-						WeaponsAPI.SwitchToWeapon(index);
+					this.setIndexSelectionState(false);
+					this.selectedIndex[1] -= 1;
+					if (this.selectedIndex[1] < 0) {
+						this.selectedIndex[0] -= 1;
+						if (this.selectedIndex[0] < 0) {
+							this.selectedIndex[0] = this.bucketedWeapons.length - 1;
+						}
+						this.selectedIndex[1] = this.bucketedWeapons[this.selectedIndex[0]].length - 1;
 					}
+					const weapon = this.bucketedWeapons[this.selectedIndex[0]][this.selectedIndex[1]].weapon;
+					$.Msg(`Select Weapon: ${weapon.classname}`);
+					this.setIndexSelectionState(true);
+					WeaponsAPI.SwitchToWeapon(WeaponsAPI.GetWeaponIndexFromClass(weapon.classname));
+					this.fadeOutTimer();
 					break;
 				}
 		
@@ -69,6 +151,7 @@ class WeaponSwitcher {
 	static updateWeapons() {
 		$.Msg('Updating weapons...');
 
+		this.bucketedWeapons = [];
 		this.weapons = WeaponsAPI.GetWeapons();
 		if (this.weapons === undefined) {
 			$.Warning('Weapons array is undefined');
@@ -80,6 +163,100 @@ class WeaponSwitcher {
 		for (const weapon of this.weapons) {
 			if (!weapon) continue;
 			$.Msg(`===== WEAPON: ${weapon.name} (${weapon.classname}), SLOT ${weapon.slot}`);
+			if (!this.bucketedWeapons[weapon.slot]) {
+				$.Msg(`Create new bucket at index: ${weapon.slot}`);
+				this.bucketedWeapons[weapon.slot] = [];
+			}
+			this.bucketedWeapons[weapon.slot].push(new SwitcherData(weapon));
 		}
+
+		this.constructPanels();
+
+		const curWep = WeaponsAPI.GetActiveWeaponInfo();
+		if (!curWep) return;
+		try {
+			this.bucketedWeapons.forEach((weapons: SwitcherData[], i: number) => {
+				weapons.forEach((weapon: SwitcherData, j: number) => {
+					if (weapon.weapon.classname === curWep.classname) {
+						this.selectedIndex[0] = i;
+						this.selectedIndex[1] = j;
+						throw 'Found';
+					}
+				});
+				$.Warning('Could not find what the active weapon is in buckets!');
+			});
+		} catch {
+			$.Msg(`Current weapon selection updated to: [${this.selectedIndex[0]}, ${this.selectedIndex[1]}]`);
+		}
+	}
+
+	static constructPanels() {
+		$.GetContextPanel().RemoveAndDeleteChildren();
+
+		this.bucketedWeapons.forEach((weapons: SwitcherData[], index: number) => {
+			const bucketPanel = $.CreatePanel('Panel', $.GetContextPanel(), `Bucket${index}`);
+			bucketPanel.AddClass('weapons__bucket');
+
+			weapons.forEach((data: SwitcherData, weaponIndex: number) => {
+				const weapon = data.weapon;
+				const weaponPanel = $.CreatePanel('Panel', bucketPanel, `Bucket${index}-${weapon.classname}`);
+				weaponPanel.AddClass('weapons__bucket__entry');
+
+				if (weaponIndex !== 0) {
+					weaponPanel.AddClass('weapons__bucket__entry__sub');
+				}
+
+				this.bucketedWeapons[index][weaponIndex].panel = weaponPanel;
+
+				if (weaponIndex === 0) {
+					const bucketLabel = $.CreatePanel('Label', weaponPanel, `Bucket${index}-NumLabel`, { 'text': `${index + 1}` });
+					bucketLabel.AddClass('weapons__bucket__entry__num');
+				}
+
+				const weaponIcon = $.CreatePanel('Label', weaponPanel, `${weapon.classname}-IconLabel`);
+				weaponIcon.AddClass('weapons__bucket__entry__icon-label');
+				switch (weapon.classname) {
+					case 'weapon_crowbar':
+						weaponIcon.text = 'c';
+						break;
+					case 'weapon_physcannon':
+						weaponIcon.text = 'm';
+						break;
+					case 'weapon_pistol':
+						weaponIcon.text = 'd';
+						break;
+					case 'weapon_357':
+						weaponIcon.text = 'e';
+						break;
+					case 'weapon_smg1':
+						weaponIcon.text = 'a';
+						break;
+					case 'weapon_ar2':
+						weaponIcon.text = 'l';
+						break;
+					case 'weapon_crossbow':
+						weaponIcon.text = 'g';
+						break;
+					case 'weapon_shotgun':
+						weaponIcon.text = 'b';
+						break;
+					case 'weapon_rpg':
+						weaponIcon.text = 'i';
+						break;
+					case 'weapon_frag':
+						weaponIcon.text = 'k';
+						break;
+					case 'weapon_bugbait':
+						weaponIcon.text = 'j';
+						break;
+					default:
+						weaponIcon.text = 'A';
+						break;
+				}
+
+				const weaponLabel = $.CreatePanel('Label', weaponPanel, `${weapon.classname}-Label`, { 'text': `${weapon.classname}` });
+				weaponLabel.AddClass('weapons__bucket__entry__name');
+			});
+		});
 	}
 }
