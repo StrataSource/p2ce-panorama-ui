420b27b9918753d723344047abccc7b9cea23568
pivotman319 <pivotman2003@gmail.com>
2026-01-21 20:06:10 +0100

    Revert to older art asset loading behavior, add {campaign_art} search path to panorama.cfg and move related art assets

--- scripts/pages/campaigns/campaign-chapters.ts
+++ scripts/pages/campaigns/campaign-chapters.ts
@@ -46,7 +46,7 @@ class ChapterEntry {
 		}
 		if (cover) {
 			const thumb = this.chapter.meta[CampaignMeta.CHAPTER_THUMBNAIL];
-			if (thumb) cover.SetImage(`file://{game}/${thumb}`);
+			if (thumb) cover.SetImage(`file://${thumb}`);
 			else cover.SetImage(getRandomFallbackImage());
 		}
 
