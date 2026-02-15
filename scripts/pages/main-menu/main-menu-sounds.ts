bfb64fba0fcbaf04157e23cb034d4a94d1794049
pivotman319 <pivotman2003@gmail.com>
2026-02-15 02:17:14 +0100

    integrate most of the silenthaven sfx

---
+++ scripts/pages/main-menu/main-menu-sounds.ts
@@ -0,0 +1,20 @@
+// ==============================================================================================================
+// Pivot: This is probably the ugliest fucking adaptation of Momentum Mod UI code I've ever done for P2:CE yet.
+// The ambience sound code located at momentum\panorama\scripts\pages\main-menu\main-menu-sounds.ts
+// has essentially been reduced to nothing but a two-function event handler.
+//
+// Somebody whack me with a trout already.
+// ==============================================================================================================
+
+$.RegisterForUnhandledEvent('PopulateLoadingScreen', () => playMapLoadMusic());
+$.RegisterForUnhandledEvent('MapLoaded', () => stopMapLoadMusic());
+
+function playMapLoadMusic() {
+	$.PlaySoundEvent('UIPanorama.P2CE.MenuFocus');
+	$.PlaySoundEvent('UIPanorama.Music.P2CE.Menu2_LOAD');
+}
+
+function stopMapLoadMusic() {
+	$.PlaySoundEvent('UIPanorama.Music.StopAll');
+	$.PlaySoundEvent('UIPanorama.P2CE.MenuAccept');
+}
