11c123969bfdbfffcb79f606a1b6ee6a8dccfa94
iamsirenstorm <120330118+iamsirenstorm@users.noreply.github.com>
2026-03-15 09:43:42 -0500

    fix: hacky reparenting shenans

--- scripts/pages/main-menu/pause-menu.ts
+++ scripts/pages/main-menu/pause-menu.ts
@@ -38,11 +38,13 @@ class PauseMenu {
 				);
 			},
 			hovered: () => {
-				if (this.continueBtn.enabled) this.continueBox.visible = true;
+				if (this.continueBtnEnabled && this.continueBox.IsValid())
+					this.continueBox.visible = true;
 			},
 			unhovered: () => {
 				if (this.continueBox.IsValid()) this.continueBox.visible = false;
 			},
+			additionalClasses: 'mainmenu__nav__btn__no-gradient',
 			focusIsHover: true
 		},
 		{
@@ -101,28 +103,16 @@ class PauseMenu {
 
 	// i copied and pasted this code, sue me
 	// maybe fix it up later...
-	static continueBtnText = $<Label>('#ContinueSaveTagline')!;
 	static continueBox = $<Panel>('#ContinueBox')!;
 	static continueBoxText = $<Label>('#ContinueSaveTagline')!;
 	static continueImg = $<Image>('#ContinueSaveThumb')!;
-	static continueBtn: Button;
+	static continueBtnEnabled: boolean = false;
+	static continueBtn: string = 'CampaignContinueBtn';
 
 	static latestSave: GameSave;
 
 	static onLoad() {
 		for (const btn of this.buttons) {
-			if (btn.id === 'CampaignContinueBtn') {
-				this.continueBtn = constructMenuButton(btn);
-				this.continueBtn.AddClass('mainmenu__nav__btn__no-gradient');
-
-				const t = this.continueBtn.FindChildTraverse('CampaignContinueBtn_Tagline');
-				if (t) this.continueBtnText = t as Label;
-				else throw new Error('Cannot find Continue Button tagline!');
-
-				$.DispatchEvent('MainMenuAddPreConstructedButton', this.continueBtn);
-				continue;
-			}
-
 			$.DispatchEvent('MainMenuAddButton', btn);
 		}
 
@@ -142,8 +132,18 @@ class PauseMenu {
 	static setContinueDetails() {
 		if (!CampaignAPI.IsCampaignActive()) {
 			this.continueBox.visible = false;
-			this.continueBtn.enabled = false;
-			this.continueBtnText.text = $.Localize('#MainMenu_SaveRestore_NoSaves');
+			this.continueBtnEnabled = false;
+			const continueBtnText = $.Localize('#MainMenu_SaveRestore_NoSaves');
+
+			$.DispatchEvent(
+				'MainMenuSetButtonProps',
+				this.continueBtn,
+				{
+					taglineText: continueBtnText,
+					enabled: this.continueBtnEnabled
+				}
+			);
+
 			$.DispatchEvent('MainMenuSetLogo', 'file://{images}/logo.svg');
 			$.DispatchEvent('MainMenuSetLogoSize', CampaignLogoSizePreset.STANDARD);
 			return;
@@ -178,8 +178,17 @@ class PauseMenu {
 			})
 			.sort((a, b) => Number(b.fileTime) - Number(a.fileTime));
 
-		this.continueBtn.enabled = false;
-		this.continueBtnText.text = $.Localize('#MainMenu_SaveRestore_NoSaves');
+		this.continueBtnEnabled = false;
+		let continueBtnText = $.Localize('#MainMenu_SaveRestore_NoSaves');
+
+		$.DispatchEvent(
+			'MainMenuSetButtonProps',
+			this.continueBtn,
+			{
+				taglineText: continueBtnText,
+				enabled: this.continueBtnEnabled
+			}
+		);
 
 		if (saves.length === 0) {
 			$.Warning('PAUSE MENU: No saves');
@@ -209,16 +218,23 @@ class PauseMenu {
 			return;
 		}
 
-		//const chapterString = `[${c.chapters.indexOf(savChapter!) + 1} / ${c.chapters.length}]`;
-
 		const thumb = `file://{__saves}/${this.latestSave.fileName.replace('.sav', '.tga')}`;
 		this.continueImg.SetImage(thumb);
 
 		const date = new Date(Number(this.latestSave.fileTime));
 		this.continueBoxText.text = convertTime(date);
 		const chapterName = isWsSingle ? c.campaign.title : $.Localize(savChapter.title);
-		this.continueBtnText.text = chapterName.replace('\n', ': ');
+		continueBtnText = chapterName.replace('\n', ': ');
 
-		this.continueBtn.enabled = true;
+		this.continueBtnEnabled = true;
+
+		$.DispatchEvent(
+			'MainMenuSetButtonProps',
+			this.continueBtn,
+			{
+				taglineText: continueBtnText,
+				enabled: this.continueBtnEnabled
+			}
+		);
 	}
 }
