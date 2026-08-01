cf74e87b7340397b3be0d832cd7cdaed76a83da8
iamsirenstorm <120330118+iamsirenstorm@users.noreply.github.com>
2026-08-01 02:48:11 -0500

    feat: MP pause files, lobby dev start buttons

--- scripts/pages/main-menu/pause-menu.ts
+++ scripts/pages/main-menu/pause-menu.ts
@@ -104,7 +104,7 @@ class PauseMenu {
 					() => {
 						GameInterfaceAPI.ConsoleCommand('quit');
 					},
-					$.Localize('#Common_Return'),
+					$.Localize('#Common_Cancel'),
 					() => {},
 					'blur'
 				);
